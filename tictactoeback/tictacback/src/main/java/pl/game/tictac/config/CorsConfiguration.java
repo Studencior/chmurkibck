4741876e9d6beab58e17dee7344f5312dd6c47b1
Daniel Proszynski <256795@student.pwr.edu.pl>
2024-01-08 11:47:58 +0100

    af

--- tictactoeback/tictacback/src/main/java/pl/game/tictac/config/CorsConfiguration.java
+++ tictactoeback/tictacback/src/main/java/pl/game/tictac/config/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://http://54.86.211.202") // Replace with the Angular app URL
+                        .allowedOrigins("http://54.86.211.202") // Replace with the Angular app URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
